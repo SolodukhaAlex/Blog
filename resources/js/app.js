aa63ac50cd24126828320dd8dbdc87dad533153e
SolodukhaAlex <solodukha.as@gmail.com>
2019-07-16 22:24:47 +0300

    before yarl

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,12 @@
 
 require('./bootstrap');
 
+$(document).ready(function(){
+    CKEDITOR.replace( 'description_short' );
+    CKEDITOR.replace( 'description' );
+
+})
+
 window.Vue = require('vue');
 
 /**
